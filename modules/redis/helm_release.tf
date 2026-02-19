bb11faea3e34bb64d61908290043ce61c8192293
Steve Tarter <steve@tarterware.com>
2026-02-19 17:15:09 -0600

    Update config to use Cognito instead of Auth0.

--- modules/redis/helm_release.tf
+++ modules/redis/helm_release.tf
@@ -2,12 +2,14 @@ resource "helm_release" "redis" {
   name       = "redis"
   namespace  = var.roadrunner_namespace
   chart      = "redis"
-  repository = "https://charts.bitnami.com/bitnami"
-  version    = "19.4.0"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  version    = "25.3.0"
 
   # Set values (equivalent to --set in Helm CLI)
-  set {
-    name  = "replica.replicaCount"
-    value = "1"
-  }
+  set = [
+    {
+      name  = "replica.replicaCount"
+      value = "1"
+    }
+  ]
 }
